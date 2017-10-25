1b62602f3ea2f7001d4389039bc1d78d95800592
Shaad Jeetoo <shaad.jeetoo@gmail.com>
2017-10-25 16:39:38 +0400

    new coomit 2

--- app/car-rental/src/app/app.module.ts
+++ app/car-rental/src/app/app.module.ts
@@ -1,17 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { BookComponent } from './book/book.component';
+import { FleetComponent } from './fleet/fleet.component';
+import { HomeComponent } from './home/home.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, BookComponent, FleetComponent, HomeComponent, AppFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    RouterModule.forRoot([
+      	{
+	        path: '',
+	    	component: HomeComponent
+    	},
+    	{
+	        path: 'home',
+	    	component: HomeComponent
+    	},
+    	{
+	        path: 'fleet',
+	    	component: FleetComponent
+    	},
+    	{
+	        path: 'book',
+	    	component: BookComponent
+    	}
+
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent, HeaderComponent]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+ }
