707395ca31df6fc2ca7d07e33776d9aeb6ff29c1
Shaad Jeetoo <shaad.jeetoo@gmail.com>
2017-10-25 14:06:00 +0400

    Initial commit

---
+++ app/car-rental/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, HeaderComponent]
+})
+export class AppModule { }
